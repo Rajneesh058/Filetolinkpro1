95bdef08a4d44e3f82780e54381b06d7d1175e66
Rajneesh058 <114851821+Rajneesh058@users.noreply.github.com>
2023-02-02 22:39:36 +0530

    Update start_help.py

--- Adarsh/bot/plugins/start_help.py
+++ Adarsh/bot/plugins/start_help.py
@@ -77,12 +77,12 @@ async def start(b, m):
     await StreamBot.send_photo(
         chat_id=m.chat.id,
         photo ="https://graph.org/file/19ad6ad1d04e540328efd.jpg",
-        caption ="""f'👋 Hᴇʏ """{m.from_user.mention(style="md")}!,\n
+        caption =f'👋 Hᴇʏ,{m.from_user.mention(style="md")}!,\n
                    """ I'ᴍ Tᴇʟᴇɢʀᴀᴍ Fɪʟᴇs Sᴛʀᴇᴀᴍɪɴɢ Bᴏᴛ ᴀs ᴡᴇʟʟ Dɪʀᴇᴄᴛ Lɪɴᴋs Gᴇɴᴇʀᴀᴛᴇ\n
                     Cʟɪᴄᴋ ᴏɴ Hᴇʟᴘ ᴛᴏ ɢᴇᴛ ᴍᴏʀᴇ ɪɴғᴏʀᴍᴀᴛɪᴏɴ\n
                     𝗪𝗔𝗥𝗡𝗜𝗡𝗚 🚸\n\n
                     🔞 Pʀᴏɴ ᴄᴏɴᴛᴇɴᴛꜱ ʟᴇᴀᴅꜱ ᴛᴏ ᴘᴇʀᴍᴀɴᴇɴᴛ ʙᴀɴ ʏᴏᴜ.\n\n
-                    🍃 Bᴏᴛ Mᴀɪɴᴛᴀɪɴᴇᴅ Bʏ :@Epic_creation_bots""",reply_markup=buttonz)
+                    🍃 Bᴏᴛ Mᴀɪɴᴛᴀɪɴᴇᴅ Bʏ :@Epic_creation_bots,reply_markup=buttonz)
 
 
 @StreamBot.on_message((filters.command("help") | filters.regex('help📚')) & filters.private )
